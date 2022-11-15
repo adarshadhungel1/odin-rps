6968b4f4c1975d0f91b01412296735b450dade57
adarshaadhungel1 <dadarsha6@gmail.com>
2022-11-15 06:59:56 +0545

    Added meaningful display results

--- script/script.js
+++ script/script.js
@@ -2,20 +2,19 @@ function startgame(){
     let options = [1,2,3]; //1 = rock, 2 = paper, 3 = scissors
     let ai = Math.floor(Math.random() * options.length);
     let choice = prompt("1 for rock, 2 for paper, 3 for scissor:");
-    if (ai==1 && choice==1){alert("tie")}
-    else if (ai==1 && choice==2){alert("win")}
-    else if (ai==1 && choice==3){alert("lose")}
+    if (ai==1 && choice==1){alert("User Chose Rock and AI chose Rock.\n Result = Tie")}
+    else if (ai==1 && choice==2){alert("User Chose Paper and AI chose Rock.\n Result = Win")}
+    else if (ai==1 && choice==3){alert("User Chose Scissor and AI chose Rock.\n Result = Lose")}
 
-    else if (ai==2 && choice==1){alert("lose")}
-    else if (ai==2 && choice==2){alert("tie")}
-    else if (ai==2 && choice==3){alert("win")}
+    else if (ai==2 && choice==1){alert("User Chose Rock and AI chose Paper.\n Result = Lose")}
+    else if (ai==2 && choice==2){alert("User Chose Paper and AI chose Paper.\n Result = Tie")}
+    else if (ai==2 && choice==3){alert("User Chose Scissor and AI chose Paper.\n Result = Win")}
 
-    else if (ai==3 && choice==1){alert("win")}
-    else if (ai==3 && choice==2){alert("lose")}
-    else if (ai==3 && choice==3){alert("tie")}
+    else if (ai==3 && choice==1){alert("User Chose Rock and AI chose Scissor.\n Result =  Win")}
+    else if (ai==3 && choice==2){alert("User Chose Paper and AI chose Scissor.\n Result = Lose")}
+    else if (ai==3 && choice==3){alert("User Chose Scissor and AI chose Scissor.\n Result = Tie")}
 
-    else {alert("something wrong")};
-}
-
+    else {alert("Unexpected Input!!")};
+    }
 
 startgame()
