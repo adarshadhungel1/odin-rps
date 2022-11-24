75f4afab065c966eb288e8e1e915f1a1fcd4b572
adarshaadhungel1 <dadarsha6@gmail.com>
2022-11-24 18:25:01 +0545

    imported game logic from existing system

--- script/script.js
+++ script/script.js
@@ -19,7 +19,21 @@ function startgame(){
 
 
 function playgame(e){
-    let selection = e.path[0].id;
+    let options = ['rock', 'paper', 'scissors'];
+    let userSelection = e.path[0].id;
+    let aiChoice = options[Math.floor(Math.random()*options.length)];
+    
+    if (aiChoice=='rock' && userSelection=='paper'){alert("User Chose Rock and aiChoiceChoice chose Rock.\n Result = Tie")}
+    else if (aiChoice=='rock' && userSelection=='paper'){alert("User Chose Paper and aiChoice chose Rock.\n Result = Win")}
+    else if (aiChoice=='rock' && userSelection=='scissors'){alert("User Chose Scissor and aiChoice chose Rock.\n Result = Lose")}
+
+    else if (aiChoice=='paper' && userSelection=='rock'){alert("User Chose Rock and aiChoice chose Paper.\n Result = Lose")}
+    else if (aiChoice=='paper' && userSelection=='paper'){alert("User Chose Paper and aiChoice chose Paper.\n Result = Tie")}
+    else if (aiChoice=='paper' && userSelection=='scissors'){alert("User Chose Scissor and aiChoice chose Paper.\n Result = Win")}
+
+    else if (aiChoice=='scissors' && userSelection=='rock'){alert("User Chose Rock and aiChoice chose Scissor.\n Result =  Win")}
+    else if (aiChoice=='scissors' && userSelection=='paper'){alert("User Chose Paper and aiChoice chose Scissor.\n Result = Lose")}
+    else if (aiChoice=='scissors' && userSelection=='scissors'){alert("User Chose Scissor and aiChoice chose Scissor.\n Result = Tie")}   
 }
 
 rockbtn = document.querySelector('#rock');
